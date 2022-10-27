a1ed9d7f9dca279a7f90f1f4aab9fa7487a7b451
Raniyah44 <rania.alotaibi@hotmail.com>
2022-10-27 11:36:33 +0300

    Initial Commit

---
+++ Test7\/Test7_App.swift
@@ -0,0 +1,17 @@
+//
+//  Test7_App.swift
+//  Test7\
+//
+//  Created by Raniyah Alotaibi on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Test7_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
